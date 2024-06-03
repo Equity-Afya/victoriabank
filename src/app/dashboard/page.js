19cb20d9e446dceb2546b9af958e5815d1226235
Victortimbwa <victortimbwa@gmail.com>
2024-06-03 09:54:20 +0300

    Resolved errors in dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,14 +1,13 @@
 import styles from "./dashboard.module.css";
 import Accounts from "../components/Accounts/Accounts";
-// import RecentTransactions from "../components/RecentTransactions/RecentTransactions";
-
+import Services from "../components/services/Services";
 export default function DashboardPage() {
 	return (
 		<div className={styles.main_dashboard}>
 			<div className={styles.main_center}>
 				<Accounts />
 				<div className={styles.main_right}>
-					{/* <Quicklinks /> // This is the Quicklinks component or cards */}
+					<Services />
 				</div>
 				<div className={styles.summary_of_accounts}>
 					{/* <RecentTransactions /> */}
