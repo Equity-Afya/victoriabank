dcd8139f805af5573c0b6473cd6c1f8b593ba729
Baraka <raphaelbaraka424@gmail.com>
2024-06-07 07:57:49 +0300

    first commit

---
+++ src/app/components/add-beneficiaries/pesalink/Pesalink.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "../within-bank/WithinBank.module.css";
+
+const Pesalink = () => {
+  return (
+    <div>
+      <span className={styles.bankdetails}>Beneficiary Bank Details</span>
+      <div className={styles.form}>
+        <div className={styles.inputFields}>
+          <div className={styles.leftinput1}>
+            <input
+              type="text"
+              placeholder="Name"
+              className={styles.inputField}
+            />
+            <input
+              type="text"
+              placeholder="Account Number"
+              className={styles.inputField}
+            />
+            <select className={styles.currencySelect}>
+              <option value="" disabled selected>
+                Choose currency
+              </option>
+              <option value="Ksh">Ksh</option>
+              <option value="Dollar">Dollar</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Re-enter Account Number"
+              className={styles.inputField}
+            />
+          </div>
+
+          <div className={styles.button1}>
+            <button className={styles.button}>ADD BENEFICIARY +</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pesalink;
